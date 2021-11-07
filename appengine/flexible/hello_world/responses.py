04102879a05f5d98f4238525d4f2e224b40ce581
pgiteam <49672865+pgiteam@users.noreply.github.com>
2021-11-07 14:14:54 +0100

    Update responses.py

--- appengine/flexible/hello_world/responses.py
+++ appengine/flexible/hello_world/responses.py
@@ -1,1 +1,14 @@
+from pywebio import *
+from pywebio.output import *
+from pywebio.input import *
 
+def main():
+    '''
+    An interactive web app that takes user's name 
+    and output hello <username> on the webpage
+    '''
+    name = input("Hello! We look forward to working with you. What’s your name?")
+    print("Nice to meet you " + name + "!")
+    appdescription = input("Please describe the nature of the mobile app you would like to have built? ")
+    print("OK, thank you " + "!") 
+    appname = input("Have you thought about an app name? Respond Yes or No ")
